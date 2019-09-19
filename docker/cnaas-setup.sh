d64046fac44aac8f0c07dc97473abfe88cb62ab0
Kristofer Hallin <kristofer@sunet.se>
2019-09-19 12:21:25 +0200

    We can now build a Docker container.

--- docker/cnaas-setup.sh
+++ docker/cnaas-setup.sh
@@ -7,24 +7,46 @@ export DEBIAN_FRONTEND noninteractive
 
 /bin/sed -i s/deb.debian.org/ftp.se.debian.org/g /etc/apt/sources.list
 
-
 apt-get update && \
     apt-get -y dist-upgrade && \
     apt-get install -y \
       git \
-      nginx \
+      python3-venv \
+      python3-pip \
+      python3-yaml \
       iputils-ping \
       procps \
       bind9-host \
       netcat-openbsd \
       net-tools \
       curl \
+      netcat \
+      nginx \
+      supervisor \
     && apt-get clean
+
+pip3 install uwsgi
+
+# Start venv
+python3 -m venv /opt/cnaas/venv
+cd /opt/cnaas/venv/
+source bin/activate
+
+/opt/cnaas/venv/bin/pip install -U pip
+
+# Fetch the code and install dependencies
+git clone https://github.com/SUNET/cnaas-httpd.git
+cd cnaas-httpd/
+# git checkout develop
+python3 -m pip install -r requirements.txt
+
+# Temporary for testing new branch
+#cd /opt/cnaas/venv/cnaas-nms/
+#git remote update
+#git fetch
+#git checkout --track origin/feature.autopush
+#python3 -m pip install -r requirements.txt
 
 #rm -rf /var/lib/apt/lists/*
 
-cd /opt/cnaas/www
-mkdir upload
 
-# this will be replaced by user defined repo at runtime if env variable GITREPO_TEMPLATES is set
-git clone https://github.com/SUNET/cnaas-nms-templates templates
