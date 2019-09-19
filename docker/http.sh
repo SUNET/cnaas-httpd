98a2906546d8f6447e7d8929c5283c079bfd4a23
Kristofer Hallin <kristofer@sunet.se>
2019-09-19 12:50:39 +0200

    Create templates/ directory and clone the templates repository if needed.

---
+++ docker/http.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$GITREPO_TEMPLATES" ]
+then
+	cd /opt/cnaas/www/templates
+	git pull
+else
+	cd /opt/cnaas/www
+	rm -rf /opt/cnaas/www/templates
+	git clone $GITREPO_TEMPLATES templates
+fi
+
+set -e
+
+nginx -g "daemon off;"
